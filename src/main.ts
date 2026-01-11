3e70e5506a58ebe8ebebfb063899b14e8d38caf7
Andrea Betancourt <andrea.betancourt@peakline.io>
2026-01-11 12:32:01 +0100

    Initial project configuration

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Ripple from 'primevue/ripple'
+import App from './App.vue'
+import router from './router'
+
+// PrimeVue styles
+import 'primevue/resources/themes/lara-light-green/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(PrimeVue)
+app.directive('ripple', Ripple)
+app.use(router);
+
+app.mount('#app')
+
